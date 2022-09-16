d952bc91d6b6f6bb2dac78acb0f3e4134308084d
Nina <nino4ka.lix@gmail.com>
2022-09-16 20:08:03 +0300

    Добавлен 1 пример

---
+++ Lesson01/Homework01/Example001/Program.cs
@@ -0,0 +1,16 @@
+﻿int numberA = -9;
+int numberB = -3;
+int max, min;
+
+if (numberA > numberB)
+{
+   max = numberA;
+   min = numberB;
+}
+else 
+{
+    max = numberB;
+    min = numberA;
+}
+ Console.WriteLine("min = " + min);
+ Console.WriteLine("max = " + max);
