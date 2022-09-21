baa2915c10dd7a42a79b51e97212877bf1658dac
Nina <nino4ka.lix@gmail.com>
2022-09-21 23:26:30 +0300

    Добавлено 3 задание

---
+++ Lesson02/Homework02/Example003/Program.cs
@@ -0,0 +1,17 @@
+﻿int NumOfDay = 8;
+if (NumOfDay >=1 & NumOfDay <= 7)
+{
+    if(NumOfDay >=6)
+    {
+        Console.WriteLine("Да");
+    }
+    
+    else
+    {
+       Console.WriteLine("Нет"); 
+    }
+}
+else
+{
+    Console.WriteLine("Неверный ввод");
+}
