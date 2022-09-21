83fbf754d51b648583824053772cdba4bc0f9b04
Nina <nino4ka.lix@gmail.com>
2022-09-21 23:25:24 +0300

    Добавлено 2 задание

---
+++ Lesson02/Homework02/Example002/Program.cs
@@ -0,0 +1,17 @@
+﻿int N = 3246791;
+
+if(N >=100)
+{
+    while( N > 999 )
+    {
+        N = N / 10;
+    }
+ 
+    int Rem = N % 10;
+    Console.WriteLine(Rem);
+}
+
+else
+{
+   Console.WriteLine("Третьей цифры нет");
+}
