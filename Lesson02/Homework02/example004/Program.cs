83e997e76735863404b5ac82a50a5d79a2182b5d
Nina <nino4ka.lix@gmail.com>
2022-09-21 23:27:49 +0300

    Добавлено 4 задание

---
+++ Lesson02/Homework02/example004/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] numbers = { 10, 20, 30, 40, 50, 60, 70 };
+
+int n = numbers.Length;
+int i = 0;
+int sum = 0;
+int SA = 0;
+
+while( i < n)
+{
+    sum = sum + numbers[i];
+    i++;
+}
+
+SA = sum / n;
+Console.WriteLine (SA);
